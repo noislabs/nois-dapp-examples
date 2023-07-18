20c9d69bd8c03b2408bf565481f73a7dd34d6a5d
Simon Warta <simon@warta.it>
2023-07-18 17:21:28 +0200

    Fix manager docs

--- contracts/randdrop/src/msg.rs
+++ contracts/randdrop/src/msg.rs
@@ -4,7 +4,7 @@ use nois::NoisCallback;
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    /// manager if none set to info.sender.
+    /// The manager address. This address can update the config and withdraw funds.
     pub manager: String,
     /// Address of the Nois proxy contract
     pub nois_proxy_address: String,
@@ -68,7 +68,7 @@ pub enum QueryMsg {
 
 #[cw_serde]
 pub struct ConfigResponse {
-    /// manager if none set to info.sender.
+    /// The manager address. This address can update the config and withdraw funds.
     pub manager: String,
     /// Address of the Nois proxy contract
     pub nois_proxy_address: String,
